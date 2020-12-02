32afc98a8c400260f2fd9895009bf6ce49e1eaa6
Moises Ariza <arizamoises@gmail.com>
2020-12-02 17:32:47 -0500

    Agrege la descripcion para el mapa usando markdown y cada punto de las lineas del mapa muestra ya las ciudades

--- app.py
+++ app.py
@@ -13,19 +13,26 @@ token = open(".mapbox_token").read()
 
 co_cities = pd.read_csv("DATASET/co.csv")
 
-co_cities = co_cities.query("admin_name in ['Meta', 'Guaviare']")
+co_cities = co_cities.query("admin_name in ['Meta', 'Tolima']")
 
 
-fig = px.line_mapbox(co_cities, lat="lat", lon="lng", color="admin_name", zoom=3, height=300, title="Intento de líneas con MAPBOX Colombia")
+fig = px.line_mapbox(co_cities, lat="lat", lon="lng", hover_name="city",color="admin_name", zoom=3, height=300)
 
 fig.update_layout(mapbox_style = "dark", mapbox_accesstoken=token, mapbox_zoom = 4, mapbox_center_lat =41,
     margin ={"r":0, "t":0, "l":0, "b":0})
 
+markdown_text = '''
+### DASHBOARD PARA SCRUM LATAM
+Este dashboard experimental se hace con el obetivo de probar las capacidades 
+que proporciona el mapa de lineas con MAPBOX
+'''
 app.layout = html.Div(children=[
+    dcc.Markdown(children=markdown_text),
     dcc.Graph(
         id='Gráfico de lineas',
         figure = fig
     )
+    
 ])
 #fig.show()
 
