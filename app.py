027a4eabb340b8cd9f015a6e41ecb97714ce9efe
Moises Ariza <arizamoises@gmail.com>
2020-12-02 13:56:49 -0500

    Agregando uso de MAPBOX a traves de su token ademas de cambiando el dataset usado

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ import plotly.express as  px
 app = dash.Dash(__name__)
 server = app.server
 
-co_cities = pd.read_csv("DATASET/departamentoscolombia.csv")
+#Agregamos nuestro token para usar MAPBOX
+token = open(".mapbox_token").read()
 
-co_cities = co_cities.query("Departamento in ['Meta', 'Guaviare']")
+co_cities = pd.read_csv("DATASET/co.csv")
+
+co_cities = co_cities.query("admin_name in ['Meta', 'Guaviare']")
 
 
-fig = px.line_mapbox(co_cities, lat="lat", lon="lon", color="Departamento", zoom=3, height=300, title="Intento de líneas con MAPBOX Colombia")
+fig = px.line_mapbox(co_cities, lat="lat", lon="lng", color="admin_name", zoom=3, height=300, title="Intento de líneas con MAPBOX Colombia")
 
-fig.update_layout(mapbox_style = "carto-darkmatter", mapbox_zoom = 4, mapbox_center_lat =41,
+fig.update_layout(mapbox_style = "dark", mapbox_accesstoken=token, mapbox_zoom = 4, mapbox_center_lat =41,
     margin ={"r":0, "t":0, "l":0, "b":0})
 
 app.layout = html.Div(children=[
