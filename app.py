17e25cea5ad5c47bb249bbe5a50260349399942e
Moises Ariza <arizamoises@gmail.com>
2020-12-02 12:27:02 -0500

    Agregando grafico de mapa con estructura basica de Dash

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import pandas as pd
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-us_cities = us_cities.query("State in ['New York', 'Ohio']")
-
+import dash 
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.express as  px
 
-fig = px.line_mapbox(us_cities, lat="lat", lon="lon", color="State", zoom=3, height=300)
+#Iniciar server
+app = dash.Dash(__name__)
+server = app.server
 
-fig.update_layout(mapbox_style = "stamen-terrain", mapbox_zoom = 4, mapbox_center_lat =41,
+co_cities = pd.read_csv("DATASET/departamentoscolombia.csv")
+
+co_cities = co_cities.query("Departamento in ['Meta', 'Guaviare']")
+
+
+fig = px.line_mapbox(co_cities, lat="lat", lon="lon", color="Departamento", zoom=3, height=300, title="Intento de líneas con MAPBOX Colombia")
+
+fig.update_layout(mapbox_style = "carto-darkmatter", mapbox_zoom = 4, mapbox_center_lat =41,
     margin ={"r":0, "t":0, "l":0, "b":0})
 
-fig.show()
+app.layout = html.Div(children=[
+    dcc.Graph(
+        id='Gráfico de lineas',
+        figure = fig
+    )
+])
+#fig.show()
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    
